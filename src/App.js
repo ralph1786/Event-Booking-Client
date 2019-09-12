a9dd28570b29e62fe456d9eb99fbc0c4902541d6
ralph1786 <ralph1786@gmail.com>
2019-09-12 16:26:10 -0400

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from "react-router-dom";
 import "./App.css";
+import AuthPage from "./pages/Auth";
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
 
 function App() {
   return (
-    <div className="App">
-      <h1>It works</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Redirect from="/" to="/auth" exact />
+        <Route path="/auth" render={() => <AuthPage />} />
+        <Route path="/events" render={() => <EventsPage />} />
+        <Route path="/bookings" render={() => <BookingsPage />} />
+      </Switch>
+    </Router>
   );
 }
 
