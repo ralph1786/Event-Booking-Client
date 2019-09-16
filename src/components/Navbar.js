dda66e09ffa3168d8b70014be9c86e7b053b4c2d
ralph1786 <ralph1786@gmail.com>
2019-09-16 12:33:48 -0400

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useContext, Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.scss";
+import AuthContext from "../context/auth-context";
+
+function Navbar() {
+  const context = useContext(AuthContext);
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation__logo">
+        <h2>EzEvents</h2>
+      </div>
+      <nav className="main-navigation__items">
+        <ul>
+          {!context.token && (
+            <li>
+              <NavLink to="/auth">LogIn</NavLink>
+            </li>
+          )}
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          {context.token && (
+            <Fragment>
+              <li>
+                <NavLink to="/bookings">Bookings</NavLink>
+              </li>
+              <li>
+                <button onClick={context.logout}>Logout</button>
+              </li>
+            </Fragment>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
