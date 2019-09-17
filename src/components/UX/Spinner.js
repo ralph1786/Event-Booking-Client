10e836e1141eb9494b1f7145d51880d80f8e4a9e
ralph1786 <ralph1786@gmail.com>
2019-09-17 17:08:41 -0400

    created a spinner

---
+++ src/components/UX/Spinner.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Spinner.scss";
+
+function Spinner() {
+  return (
+    <div className="spinner__container">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Spinner;
