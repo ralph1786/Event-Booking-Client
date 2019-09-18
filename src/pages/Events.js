bfb8ed39c3709df9397817661669b52981ff8f9e
ralph1786 <ralph1786@gmail.com>
2019-09-18 13:15:01 -0400

    page to see all events, its details, book event

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,7 +1,245 @@
-import React from "react";
+import React, {
+  Fragment,
+  useState,
+  useRef,
+  useContext,
+  useEffect
+} from "react";
+import "./Events.scss";
+import Modal from "../components/Modal";
+import Backdrop from "../components/Backdrop";
+import EventList from "../containers/EventList";
+import axios from "axios";
+import AuthContext from "../context/auth-context";
+import Spinner from "../components/UX/Spinner";
 
 function Events() {
-  return <div>Events Page</div>;
+  //state
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  //refs
+  const titleRef = useRef("");
+  const priceRef = useRef(0);
+  const dateRef = useRef("");
+  const descriptionRef = useRef("");
+
+  //context
+  const context = useContext(AuthContext);
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedEvent(null);
+  };
+
+  const fetchEvents = () => {
+    setIsLoading(true);
+    const requestBody = {
+      query: `
+        query {
+          events{
+            _id
+            title
+            description
+            date
+            price
+            creator {
+              _id
+              email
+            }
+          }
+        }`
+    };
+
+    axios
+      .post("http://localhost:4000/graphql", requestBody)
+      .then(data => {
+        setIsLoading(false);
+        console.log(data);
+        const listOfEvents = data.data.data.events;
+        setEvents(listOfEvents);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const createEvent = () => {
+    setIsModalOpen(false);
+    const title = titleRef.current.value;
+    const price = +priceRef.current.value; //the + sign turns it into a number
+    const date = dateRef.current.value;
+    const description = descriptionRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      price <= 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+    const requestBody = {
+      query: `
+        mutation CreateEvent($title: String!, $description: String!, $price: Float!, $date: String!) {
+          createEvent(eventInput: {title: $title, description: $description, price: $price, date: $date}){
+            _id
+            title
+            description
+            date
+            price
+            creator {
+              _id
+            }
+          }
+        }`,
+      variables: {
+        title: title,
+        description: description,
+        price: price,
+        date: date
+      }
+    };
+
+    axios
+      .post("http://localhost:4000/graphql", requestBody, {
+        headers: {
+          Authorization: `Bearer ${context.token}`
+        }
+      })
+      .then(data => {
+        // console.log(data);
+        const createdEvent = data.data.data.createEvent;
+        // console.log(createdEvent);
+        setEvents([...events, createdEvent]);
+      })
+      .catch(err => console.log(err));
+  };
+
+  const showEventDetails = eventId => {
+    const chosenEvent = events.find(event => event._id === eventId);
+    setSelectedEvent(chosenEvent);
+  };
+
+  const bookEvent = () => {
+    if (!context.token) {
+      setSelectedEvent(null);
+      return;
+    }
+    setIsLoading(true);
+    const requestBody = {
+      query: `
+        mutation BookEvent($id: ID!){
+          bookEvent (eventId: $id){
+            _id
+            createdAt
+            updatedAt
+          }
+        }`,
+      variables: {
+        id: selectedEvent._id
+      }
+    };
+
+    axios
+      .post("http://localhost:4000/graphql", requestBody, {
+        headers: {
+          Authorization: `Bearer ${context.token}`
+        }
+      })
+      .then(data => {
+        console.log(data);
+        setIsLoading(false);
+        setSelectedEvent(null);
+      })
+      .catch(err => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  };
+
+  return (
+    <Fragment>
+      {(isModalOpen || selectedEvent) && <Backdrop />}
+      {isModalOpen && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onCancel={closeModal}
+          onConfirm={createEvent}
+          confirmText="Create"
+        >
+          <form>
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" ref={titleRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" id="price" ref={priceRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="datetime-local" id="date" ref={dateRef} />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea
+                id="description"
+                cols="30"
+                rows="10"
+                ref={descriptionRef}
+              ></textarea>
+            </div>
+          </form>
+        </Modal>
+      )}
+      {selectedEvent && (
+        <Modal
+          title={selectedEvent.title}
+          canCancel
+          canConfirm
+          onCancel={closeModal}
+          onConfirm={bookEvent}
+          confirmText="Book"
+        >
+          <h2>{selectedEvent.title}</h2>
+          <h4>${selectedEvent.price}</h4>
+          <h4>Date: {new Date(selectedEvent.date).toLocaleDateString()}</h4>
+          <p>{selectedEvent.description}</p>
+        </Modal>
+      )}
+
+      {context.token && (
+        <div className="create-event">
+          <p>Add new events to our platform!</p>
+          <button
+            className="create-event__btn"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Create Event
+          </button>
+        </div>
+      )}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <EventList
+          events={events}
+          authUserId={context.userId}
+          onViewDetail={showEventDetails}
+        />
+      )}
+    </Fragment>
+  );
 }
 
 export default Events;
