b7bbfa5cb80d9f52fae564240fbb391b986a363b
ralph1786 <ralph1786@gmail.com>
2019-09-18 13:11:46 -0400

    modified graphql queries

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -16,25 +16,33 @@ function Auth() {
     }
     let requestBody = {
       query: `
-        query {
-          login(email: "${email}", password: "${password}"){
+        query LoginUser($email: String!, $password: String!){
+          login(email: $email, password: $password){
             userId
             token
             tokenExpiration
           }
         }
-      `
+      `,
+      variables: {
+        email: email,
+        password: password
+      }
     };
 
     if (!loginMode) {
       requestBody = {
         query: `
-        mutation {
-          createUser(userInput: {email: "${email}", password: "${password}"}){
+        mutation CreateUser($email: String!, $password: String!) {
+          createUser(userInput: {email: $email, password: $password}){
             _id
             email
           }
-        }`
+        }`,
+        variables: {
+          email: email,
+          password: password
+        }
       };
     }
     axios
