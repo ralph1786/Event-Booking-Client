f78242ded6998cd826e518a0ad25d2d5c7279667
ralph1786 <ralph1786@gmail.com>
2019-09-25 16:06:08 -0400

    added poster image

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./EventCard.scss";
 
 export default function EventCard(props) {
-  const { _id, title, price, date } = props.event;
+  const { _id, title, price, date, poster } = props.event;
   return (
     <li className="event__list__item" key={_id}>
       <div>
+        <img src={poster} alt={`${title} poster`} />
         <h2>{title}</h2>
         <h4>${price}</h4>
         <h4>Date: {new Date(date).toLocaleDateString()}</h4>
