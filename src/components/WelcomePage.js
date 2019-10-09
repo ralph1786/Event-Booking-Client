82e5e1febaa962c507d3cc4fea420d31b42372e4
ralph1786 <ralph1786@gmail.com>
2019-10-09 17:27:13 -0400

    created welcome page

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./WelcomePage.scss";
+
+function WelcomePage() {
+  return (
+    <Fragment>
+      <h1 className="welcome_message">Welcome To EzEvents!</h1>
+    </Fragment>
+  );
+}
+
+export default WelcomePage;
