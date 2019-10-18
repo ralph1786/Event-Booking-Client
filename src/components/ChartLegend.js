47c8f543a8713a10a47ab74e288c48e8b706ee7f
ralph1786 <ralph1786@gmail.com>
2019-10-18 16:12:07 -0400

    fixed spelling, added scss import

--- src/components/ChartLegend.js
+++ src/components/ChartLegend.js
@@ -1,12 +1,19 @@
 import React from "react";
+import "./ChartLegend.scss";
 
 function ChartLegend() {
   return (
     <div className="price-range">
-      <h4>Price Ranges</h4>
-      <p>Lowest-Tier-Events: $0-$49.99</p>
-      <p>Mid-Tier-Events: $50.00-$149.99</p>
-      <p>Upper-Tier-Events: $150.00-$500.00</p>
+      <h2>Price Range</h2>
+      <p>
+        Lowest-Tier-Events: <span>$0-$49.99</span>
+      </p>
+      <p>
+        Mid-Tier-Events: <span>$50.00-$149.99</span>
+      </p>
+      <p>
+        Upper-Tier-Events: <span>$150.00-$500.00</span>
+      </p>
     </div>
   );
 }
