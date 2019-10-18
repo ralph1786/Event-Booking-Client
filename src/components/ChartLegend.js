ed7aebe34917467246952c6babde3c865999368e
ralph1786 <ralph1786@gmail.com>
2019-10-18 15:26:40 -0400

    fixed spelling, added className

--- src/components/ChartLegend.js
+++ src/components/ChartLegend.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function ChartLegend() {
   return (
-    <div>
+    <div className="price-range">
       <h4>Price Ranges</h4>
       <p>Lowest-Tier-Events: $0-$49.99</p>
-      <p>Lowest-Tier-Events: $50.00-$149.99</p>
-      <p>Lowest-Tier-Events: $150.00-$500.00</p>
+      <p>Mid-Tier-Events: $50.00-$149.99</p>
+      <p>Upper-Tier-Events: $150.00-$500.00</p>
     </div>
   );
 }
