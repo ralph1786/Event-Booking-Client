820960573aa6af7c1ab362943fa0002bbb31cae2
ralph1786 <ralph1786@gmail.com>
2019-10-18 15:20:27 -0400

    shows price ranges of events tiers

---
+++ src/components/ChartLegend.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ChartLegend() {
+  return (
+    <div>
+      <h4>Price Ranges</h4>
+      <p>Lowest-Tier-Events: $0-$49.99</p>
+      <p>Lowest-Tier-Events: $50.00-$149.99</p>
+      <p>Lowest-Tier-Events: $150.00-$500.00</p>
+    </div>
+  );
+}
+
+export default ChartLegend;
