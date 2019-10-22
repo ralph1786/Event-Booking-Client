974a769fe04550003b7a1431d10aab19a3514c14
ralph1786 <ralph1786@gmail.com>
2019-10-22 13:41:31 -0400

    added error message on backdrop

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Message from "./UX/Message";
 import "./Backdrop.scss";
 
 function Backdrop({ closeModal, errorMessage }) {
   return (
     <div onClick={() => closeModal(false)} className="backdrop">
-      {errorMessage ? <p>{errorMessage}</p> : ""}
+      {errorMessage ? <Message message={errorMessage} /> : null}
     </div>
   );
 }
