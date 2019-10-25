cbf07f53ac9611b225bff3df4d79099c8b4c70a4
ralph1786 <ralph1786@gmail.com>
2019-10-25 15:29:08 -0400

    added ability to close message

--- src/components/Backdrop.js
+++ src/components/Backdrop.js
@@ -2,10 +2,12 @@ import React from "react";
 import Message from "./UX/Message";
 import "./Backdrop.scss";
 
-function Backdrop({ closeModal, errorMessage }) {
+function Backdrop({ closeModal, errorMessage, closeMessage }) {
   return (
     <div onClick={() => closeModal(false)} className="backdrop">
-      {errorMessage ? <Message message={errorMessage} /> : null}
+      {errorMessage ? (
+        <Message message={errorMessage} closeMessage={closeMessage} />
+      ) : null}
     </div>
   );
 }
