e90da123aebdb5bd5386755fd579e1745eaa09ed
ralph1786 <ralph1786@gmail.com>
2019-10-25 15:34:13 -0400

    added X to close message

--- src/components/UX/Message.js
+++ src/components/UX/Message.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./Message.scss";
 
 function Message(props) {
-  const { message, messageType } = props;
+  const { message, messageType, closeMessage } = props;
   return (
     <span
       className={
         messageType === "success" ? "success_message" : "error_message"
       }
     >
-      {message}
+      {message} <button onClick={() => closeMessage()}>X</button>
     </span>
   );
 }
