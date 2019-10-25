c86243a452821e743af252bd99a3167747de5466
ralph1786 <ralph1786@gmail.com>
2019-10-25 15:35:14 -0400

    added event filter by title

--- src/containers/EventList.js
+++ src/containers/EventList.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import EventCard from "../components/EventCard";
+import AuthContext from "../context/auth-context";
 import "./EventList.scss";
 
 function EventList(props) {
   const { events, authUserId } = props;
+  const context = useContext(AuthContext);
 
-  const eventsList = events.map(event => (
-    <EventCard
-      key={event._id}
-      event={event}
-      userId={authUserId}
-      creatorId={event.creator._id}
-      onDetail={props.onViewDetail}
-      deleteEvent={props.deleteEvent}
-    />
-  ));
+  const eventsList = events
+    .filter(event =>
+      event.title.toLowerCase().includes(context.searchTerm.toLowerCase())
+    )
+    .map(event => (
+      <EventCard
+        key={event._id}
+        event={event}
+        userId={authUserId}
+        creatorId={event.creator._id}
+        onDetail={props.onViewDetail}
+        deleteEvent={props.deleteEvent}
+      />
+    ));
   return (
     <section className="event__list">
       <ul>{eventsList}</ul>
