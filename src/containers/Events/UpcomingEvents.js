0d0281ca9dfd1d2caade6413dbc0991fc6480309
ralph1786 <ralph1786@gmail.com>
2020-01-29 12:49:10 -0500

    created component that renders upcoming events

---
+++ src/containers/Events/UpcomingEvents.js
@@ -0,0 +1,43 @@
+import React, { Fragment, useContext } from "react";
+import EventCard from "../../components/Cards/EventCard";
+import AuthContext from "../../context/auth-context";
+
+function UpcomingEvents(props) {
+  const { upcomingEvents, authUserId, onViewDetail, deleteEvent } = props;
+
+  const context = useContext(AuthContext);
+
+  const upcomingEventsList = upcomingEvents
+    .filter(event =>
+      event.title.toLowerCase().includes(context.searchTerm.toLowerCase())
+    )
+    .map(event => (
+      <EventCard
+        key={event._id}
+        event={event}
+        userId={authUserId}
+        creatorId={event.creator._id}
+        onDetail={onViewDetail}
+        deleteEvent={deleteEvent}
+      />
+    ));
+
+  return (
+    <Fragment>
+      <h2>Upcoming Events</h2>
+      <section className="event__list__upcomingEvents">
+        <ul>{upcomingEventsList}</ul>
+      </section>
+    </Fragment>
+  );
+}
+
+const arePropsEqual = (prevProps, nextProps) => {
+  //   console.log("From memo", prevProps, nextProps);
+  return (
+    prevProps.upcomingEvents === nextProps.upcomingEvents &&
+    prevProps.authUserId === nextProps.authUserId
+  );
+};
+
+export default React.memo(UpcomingEvents, arePropsEqual);
